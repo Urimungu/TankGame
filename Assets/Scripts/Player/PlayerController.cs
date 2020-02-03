e481b44eb77dcae07d68af4f7c551065be33cd7a
Uriel Mungiua <urimungu@uat.edu>
2020-02-03 13:30:12 -0700

    Fixed Player Controller Issues
    Something

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
     private Rigidbody rb;
-    private TankData TS;
+    private TankData ThisTrans;
 
     //Initializes variables
     private void Awake(){
         GameManager.GM.Player = gameObject;
         rb = GetComponent<Rigidbody>();
-        TS = GameManager.GM.transform.GetComponent<TankData>();
+        ThisTrans = GameManager.GM.transform.GetComponent<TankData>();
     }
 
     private void Update()
     {
         //Moves the tank forward if able to
-        if (TS.TankCanMove){
+        if (ThisTrans.TankCanMove){
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             Movement(horizontal, vertical);
@@ -29,20 +29,20 @@ public class PlayerController : MonoBehaviour{
         Vector2 MoveDirection = new Vector2(hor, ver).normalized;
 
         //Moves the player forwards and right
-        rb.AddForce(transform.forward * TS.TankAcc * MoveDirection.y, ForceMode.Acceleration);
-        rb.AddForce(transform.right * TS.TankAcc * MoveDirection.x, ForceMode.Acceleration);
+        rb.AddForce(transform.forward * ThisTrans.TankAcc * MoveDirection.y, ForceMode.Acceleration);
+        rb.AddForce(transform.right * ThisTrans.TankAcc * MoveDirection.x, ForceMode.Acceleration);
 
         //Makes the speed different if the player is moving backwards
-        float SpeedCap = ver > 0.1f ? TS.MaxSpeed : TS.ReverseMax;
+        float SpeedCap = ver > 0.1f ? ThisTrans.MaxSpeed : ThisTrans.ReverseMax;
 
         //Slows the player down if they are going too fast
         if (Mathf.Abs(rb.velocity.magnitude) > SpeedCap) {
-            rb.AddForce(-rb.velocity.normalized * TS.MaxSpeed);
+            rb.AddForce(-rb.velocity.normalized * ThisTrans.MaxSpeed);
         }
 
         //Slowly comes to a stop
         if (Mathf.Abs(rb.velocity.magnitude) > 0.2f && Mathf.Abs(ver) < 0.1f && Mathf.Abs(hor) < 0.1f) {
-            rb.AddForce(rb.velocity.normalized * -TS.StopSpeed);
+            rb.AddForce(rb.velocity.normalized * -ThisTrans.StopSpeed);
         }
 
     }
