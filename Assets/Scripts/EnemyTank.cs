77c937c352b17feab7b383e1054243971bbb4e55
Uriel Munguia <Urimungu@uat.edu>
2020-02-13 13:29:47 -0800

    Updated Obstacle Avoidance
    Updated the enemy tank avoiding obstacles.

--- Assets/Scripts/EnemyTank.cs
+++ Assets/Scripts/EnemyTank.cs
@@ -55,28 +55,39 @@ public class EnemyTank : MonoBehaviour
 
     private void Patrol() {
         //Moves
-        Movement();
-
-        //Looks at Way-point
-        if(WayPoints.Length > 0)
-            Cannon.transform.LookAt(WayPoints[_currentWayPoint].position, Vector3.up);
+        Movement(WayPoints[_currentWayPoint]);
 
         //Selects a new Way-point if they already reached the one they needed to get to
         if (Mathf.Abs((transform.position - WayPoints[_currentWayPoint].position).magnitude) < 0.5f){
             WayPoints[_currentWayPoint] = WayPoints[RandomSelector(WayPoints.Length - 1)];
+            _newTargetPosition = Vector3.zero;
         }
     }
 
     //Moves the Tank from point A to point B
-    private void Movement() {
+    private void Movement(Transform target) {
         //Moves towards target
-        var newDirection = NewDirection() * _data.TankSpeed;
+        var newDirection = NewDirection(target) * _data.TankSpeed;
         _rigidBody.velocity = new Vector3(newDirection.x, _rigidBody.velocity.y, newDirection.z);
-        _transform.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
+
+        //Rotates properly
+        if(_newTargetPosition == Vector3.zero) {
+            Vector3 newRotate = new Vector3(target.position.x, 0, target.position.z) 
+                - new Vector3(_transform.position.x, 0, _transform.position.z);
+            _transform.rotation = Quaternion.LookRotation(newRotate, Vector3.up);
+            //Looks at Way-point
+            Cannon.transform.LookAt(target.position, Vector3.up);
+            return;
+        }
+
+        Vector3 newRotation = new Vector3(_newTargetPosition.x, 0, _newTargetPosition.z) - new Vector3(_transform.position.x, 0, _transform.position.z);
+        _transform.rotation = Quaternion.LookRotation(newRotation.normalized, Vector3.up);
+        Cannon.transform.LookAt(_newTargetPosition, Vector3.up);
 
     }
 
-    private Vector3 NewDirection()
+    //Moves towards the desired position while avoiding obstacles
+    private Vector3 NewDirection(Transform target)
     {
         //If the Tank has reached its temporary Target Position
         if ((_transform.position - new Vector3(_newTargetPosition.x, _transform.position.y, _newTargetPosition.z)).magnitude < 0.5f)
@@ -85,43 +96,46 @@ public class EnemyTank : MonoBehaviour
         //If the Tank hasn't reached his Obstacle Avoidance Position yet, then keep going until he does
         if (_newTargetPosition != Vector3.zero)
             return (_newTargetPosition - _transform.position).normalized;
-        
+
 
         //If there is something in front of the Tank then Change Course
-        if (Physics.Raycast(_transform.position, _transform.forward, out var hit, _data.AvoidDistance, _data.AvoidMask)) {
+        Vector3 center = _transform.position + _transform.GetComponent<CapsuleCollider>().center;
+        if (Physics.Raycast(center, (target.position - _transform.position).normalized, out var hit, _data.SeeDistance, _data.AvoidMask)) {
             if (hit.collider != null) {
                 //If there is a wall in front of the Enemy 
                 Vector3 checkRight = RightOrLeft(hit.collider);
                 Vector3 checkLeft = RightOrLeft(hit.collider, false);
 
+                //If the path, seems impossible the Tank gives up and tries something else
                 if (checkRight == Vector3.zero && checkLeft == Vector3.zero) {
                     _currentWayPoint = RandomSelector(WayPoints.Length - 1);
+                    _newTargetPosition = Vector3.zero;
                     return Vector3.zero;
                 }
 
-                float rightLength = (_transform.position - checkRight).magnitude + (checkRight - WayPoints[_currentWayPoint].position).magnitude;
-                float leftLength =  (_transform.position - checkLeft).magnitude + (checkLeft - WayPoints[_currentWayPoint].position).magnitude; ;
+                //If both are viable options, it chooses the shortest route
+                float rightLength = (_transform.position - checkRight).magnitude + (checkRight - target.position).magnitude;
+                float leftLength =  (_transform.position - checkLeft).magnitude + (checkLeft - target.position).magnitude; ;
 
                 //Sets the new Target location to the new point
                 if (rightLength < leftLength) {
                     Vector3 newForward = (checkRight - _transform.position).normalized;
-                    _newTargetPosition = checkRight + (new Vector3(newForward.z, newForward.y, newForward.x) * _data.AvoidDistance) + (newForward * _data.OpeningDistance);
-                }
-                if(rightLength > leftLength){
+                    _newTargetPosition = checkRight + (_transform.right * _data.AvoidDistance) + (newForward * _data.OpeningDistance);
+                }else{
                     Vector3 newForward = (checkLeft - _transform.position).normalized;
-                    _newTargetPosition = checkLeft - (new Vector3(newForward.z, newForward.y, newForward.x) * _data.AvoidDistance) + (newForward * _data.OpeningDistance);
+                    _newTargetPosition = checkLeft - (_transform.right * _data.AvoidDistance) + (newForward * _data.OpeningDistance);
                 }
 
+                //Tries to keep everything level
                 _newTargetPosition.y = _transform.position.y;
                 return (_newTargetPosition - _transform.position).normalized;
             }
         }
 
         //Returns Regular Forward if nothing is in the way
-        return (WayPoints[_currentWayPoint].position - _transform.position).normalized;
+        return (target.position - _transform.position).normalized;
     }
-
-    //False is Left, True is Right
+    //Chooses to see if it's better to go right or left
     private Vector3 RightOrLeft(Collider hit, bool right = true) {
 
         //Initializes variables
@@ -136,6 +150,7 @@ public class EnemyTank : MonoBehaviour
                 return lastPos;
 
             lastPos = currentHit.point;
+            lastPos.y = _transform.position.y;
             angle += 0.25f * (right ? 1 : -1);
         }
 
@@ -160,7 +175,7 @@ public class EnemyTank : MonoBehaviour
     private int RandomSelector(int max) {
         int randomNumber = Random.Range(0, max);
         if(_currentWayPoint == randomNumber) {
-            if(_currentWayPoint == WayPoints.Length)
+            if(_currentWayPoint == WayPoints.Length - 1)
                 randomNumber--;
             else
                 randomNumber++;
